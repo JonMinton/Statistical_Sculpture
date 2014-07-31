c4e292c89f17840a36b8fa7219bcb7d7a2d388b0
Jonathan Minton <nate.minton@gmail.com>
2014-07-31 09:27:53 +0100

    start work on log_difs. Missing value error...

--- script.r
+++ script.r
@@ -17,7 +17,154 @@ RequiredPackages(
 counts <- read.csv("data/tidy/counts.csv")
 
 
-###########################################################################
+#########################################################################################
+#########################################################################################
+## To Automate the process ##############################################################
+
+
+#########################################################################################
+#### Mortality rates: log and identity scale
+#########################################################################################
+
+#
+fn <- function(x){
+  ages <- x$age
+  x$age <- NULL
+  x <- as.matrix(x)
+  x - min(x)
+  rownames(x) <- ages
+  return(x)
+}
+
+# automated:
+stl_spooler <- function(x, min_age=0, max_age = 80){
+  x <- subset(x, age >= min_age & age <= max_age)
+  x <- mutate(
+    x, 
+    death_rate = death_count / population_count,
+    ldeath_rate = log(death_rate)
+  )
+  
+  x_rate <- recast(
+    subset(x, select=c("year" ,"age" ,"death_rate")),
+    age ~ year,
+    id.var=c("age", "year"),
+    measure="death_rate"
+    )
+  
+  x_lrate <- recast(
+    subset(x, select=c("year", "age", "ldeath_rate")), 
+    age ~ year, 
+    id.var=c("age", "year"), 
+    measure="ldeath_rate"
+  )
+  
+  x_rate <- fn(x_rate)
+  x_lrate <- fn(x_lrate)
+  
+  r2stl(
+    x=as.numeric(rownames(x_rate)),
+    y=as.numeric(colnames(x_rate)),
+    z=x_rate,
+    
+    filename=paste0(
+      "stl/identity/death_rate_",
+      x$country[1], "_",
+      x$sex[1], ".stl"
+      ),
+    z.expand=T,
+    show.persp=F
+  )
+
+  r2stl(
+    x=as.numeric(rownames(x_lrate)),
+    y=as.numeric(colnames(x_lrate)),
+    z=x_lrate,
+    
+    filename=paste0(
+      "stl/log/ldeath_rate_",
+      x$country[1], "_",
+      x$sex[1], ".stl"
+    ),
+    z.expand=T,
+    show.persp=F
+  )
+  
+}
+
+
+d_ply(
+  counts, 
+  .(country, sex),
+  stl_spooler,
+  .progress="text"
+  )
+
+
+#########################################################################################
+#### Log mortality rate differences
+#########################################################################################
+
+#
+fn <- function(x){
+  ages <- x$age
+  x$age <- NULL
+  x <- as.matrix(x)
+  x - min(x)
+  rownames(x) <- ages
+  return(x)
+}
+
+# automated:
+stl_dif_spooler <- function(x, min_age=0, max_age = 80){
+  x <- subset(x, age >= min_age & age <= max_age)
+  this.country <- as.character(x$country[1])
+  x <- mutate(
+    x, 
+    death_rate = death_count / population_count,
+    ldeath_rate = log(death_rate)
+  )
+  
+  x <- subset(x, select=c("year", "age", "sex", "ldeath_rate"))
+  x <- recast(x, id.var=c("year", "age", "sex"), measure.var="ldeath_rate", formula=year + age ~ sex)
+  x <- mutate(x, ldif=male-female)
+  class(x) <- "data.frame" # usual issue with cast_df attribute messing things up
+
+  x <- subset(x, select=c("year", "age", "ldif"))
+
+  x <- recast(x,
+              age ~ year,
+              id.var=c("age", "year"),
+              measure="ldif"
+              )
+  browser()
+  
+  x <- fn(x)
+  browser()
+  
+  r2stl(
+    x=as.numeric(rownames(x)),
+    y=as.numeric(colnames(x)),
+    z=x,
+    filename=paste0(
+      "stl/ldif/dif_ldeath_rate_",
+      this.country, ".stl"
+      )
+    )
+  
+}
+
+
+d_ply(
+  counts, 
+  .(country),
+  stl_dif_spooler,
+  .progress="text"
+)
+
+####################################################################################
+### EAST AND WEST GERMANY ONLY #####################################################
+####################################################################################
 
 #DEUTE - east germany
 #DEUTW - west germany
@@ -35,34 +182,34 @@ east_germany <- mutate(
   east_germany, 
   death_rate = death_count / population_count,
   ldeath_rate = log(death_rate)
-  )
+)
 
 west_germany <- mutate(
   west_germany, 
   death_rate = death_count / population_count,
   ldeath_rate = log(death_rate)
-  )
+)
 
 east_f_matrix <- recast(
   subset(east_germany, subset=sex=="female", select=c("year", "age", "ldeath_rate")), 
   age ~ year, 
   id.var=c("age", "year"), 
   measure="ldeath_rate"
-  )
+)
 
 east_m_matrix <- recast(
   subset(east_germany, subset=sex=="male", select=c("year", "age", "ldeath_rate")), 
   age ~ year, 
   id.var=c("age", "year"), 
   measure="ldeath_rate"
-  )
+)
 
 west_f_matrix <- recast(
   subset(west_germany, subset=sex=="female", select=c("year", "age", "ldeath_rate")), 
   age ~ year, 
   id.var=c("age", "year"), 
   measure="ldeath_rate"
-  )
+)
 
 west_m_matrix <- recast(
   subset(west_germany, subset=sex=="male", select=c("year", "age", "ldeath_rate")), 
@@ -141,135 +288,3 @@ counts_ita <- mutate(counts_ita, death_rate = death_count/population_count)
 
 tmp <- subset(counts_ita, subset=sex=="female" & age <=80, select=c("year", "age", "death_rate"))
 tmp2 <- recast(tmp, year ~ age, measure.var="death_rate")
-
-
-##################################################################################
-
-fn <- function(x){
-  ages <- x$age
-  x$age <- NULL
-  x <- as.matrix(x)
-  x - min(x)
-  rownames(x) <- ages
-  return(x)
-}
-
-# automated:
-stl_spooler <- function(x, min_age=0, max_age = 80){
-  x <- subset(x, age >= min_age & age <= max_age)
-  x <- mutate(
-    x, 
-    death_rate = death_count / population_count,
-    ldeath_rate = log(death_rate)
-  )
-  
-  x_rate <- recast(
-    subset(x, select=c("year" ,"age" ,"death_rate")),
-    age ~ year,
-    id.var=c("age", "year"),
-    measure="death_rate"
-    )
-  
-  x_lrate <- recast(
-    subset(x, select=c("year", "age", "ldeath_rate")), 
-    age ~ year, 
-    id.var=c("age", "year"), 
-    measure="ldeath_rate"
-  )
-  
-  x_rate <- fn(x_rate)
-  x_lrate <- fn(x_lrate)
-  
-  r2stl(
-    x=as.numeric(rownames(x_rate)),
-    y=as.numeric(colnames(x_rate)),
-    z=x_rate,
-    
-    filename=paste0(
-      "stl/identity/death_rate_",
-      x$country[1], "_",
-      x$sex[1], ".stl"
-      ),
-    z.expand=T,
-    show.persp=F
-  )
-
-  r2stl(
-    x=as.numeric(rownames(x_lrate)),
-    y=as.numeric(colnames(x_lrate)),
-    z=x_lrate,
-    
-    filename=paste0(
-      "stl/log/ldeath_rate_",
-      x$country[1], "_",
-      x$sex[1], ".stl"
-    ),
-    z.expand=T,
-    show.persp=F
-  )
-  
-}
-
-
-d_ply(
-  counts, 
-  .(country, sex),
-  stl_spooler,
-  .progress="text"
-  )
-
-
-
-######################################################################################################
-## 5/2/2014
-## Work on creating STL file for 3d printing
-######################################################################################################
-# EPA Graphics
-
-# Prep work
-rm(list=ls())
-current.wd <- "X:/REFERENCE/1918 Cohort/1918/main/Data/"
-setwd(current.wd)
-source("Functions.R")
-load("DerivedData.rData")
-
-# Logged surface plot
-Make3dPlot(DeathRates,country="ITA", sex="male", log.it=T, return.valmat=T)
-
-# Want to return the output of the rgl persp3d function called in Make3dPlot
-# col="lightgrey" 
-# specular="black" 
-# axes=F 
-# box=F 
-# xlab="" 
-# ylab="" 
-# zlab="" 
-# 
-# tmp2 <- persp3d(tmp,
-#         col=col,
-#         specular=specular, axes=axes, 
-#         box=box, xlab=xlab, ylab=ylab, zlab=zlab)    
-
-require("r2stl")
-
-z <- tmp
-x <- 1:nrow(tmp)
-y <- 1:ncol(tmp)
-
-z <- z - min(z)
-
-z <- z / max(z)
-x <- x - min(x) ; x <- x / max(x)
-y <- y - min(y) ; y <- y / max(y)
-
-
-
-r2stl(
-  x=x,
-  y=y,
-  z=z,
-  
-  filename="Italy_Males_Logged_Mort.stl",
-  z.expand=T,
-  show.persp=T
-)
