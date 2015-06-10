9be23a6514569472330cc016fc81f52ba38c39c5
Jonathan Minton <nate.minton@gmail.com>
2015-06-10 20:19:10 +0100

    produce lexis plot figs

--- script.r
+++ script.r
@@ -12,6 +12,10 @@ require(r2stl)
 
 require(rgl)
 require(ggplot2)
+require(lattice)
+require(latticeExtra)
+
+
 
 
 # Load Data ---------------------------------------------------------------
@@ -611,3 +615,273 @@ d_ply(
   fn,
   .progress="text"
 )
+
+
+# Figures -----------------------------------------------------------------
+
+dir.create("figures/hmd/population/", recursive = TRUE)
+dir.create("figures/hmd/lmort/", recursive =TRUE)
+dir.create("figures/hmd/mort/", recursive =TRUE)
+
+spool_pop_figs <- function(x){
+  this_country <- x$country[1]
+  min_year <- min(x$year)
+  max_year <- max(x$year)
+  
+  title_label <- paste0(
+    this_country, " (", min_year, " - ", max_year, ")"
+  )
+  
+  file_label <- paste0(
+    "population_", this_country, "_(", min_year, "_", max_year, ")" 
+  )
+  
+  png(paste0("figures/hmd/population/", file_label, ".png"),
+      res=300, width=40, height=20, units = "cm"
+  )
+  
+  
+  
+  p <- x %>% filter( age <= 90 & sex !="total") %>% 
+    mutate(population_count = population_count/ 1000) %>% 
+  contourplot(
+    population_count ~ year * age | sex, 
+    data=. , 
+    region=T, 
+    par.strip.text=list(cex=1.4, fontface="bold"),
+    ylab=list(label="Age in years", cex=1.4),
+    xlab=list(label="Year", cex=1.4),
+    cex=1.4,
+    cuts=20,
+    col.regions=colorRampPalette(brewer.pal(6, "Blues"))(200),
+    main=NULL,
+    labels=list(cex=1.2),
+    col="darkgreen",
+    scales=list(
+      x=list(cex=1.4), 
+      y=list(cex=1.4),
+      alternating=3
+    )
+  )
+  
+  print(p)
+  dev.off()
+  
+  return(NULL)
+}
+
+
+d_ply(dta_hmd, .(country), spool_pop_figs)
+
+
+spool_mort_figs <- function(x){
+  this_country <- x$country[1]
+  min_year <- min(x$year)
+  max_year <- max(x$year)
+  
+  title_label <- paste0(
+    this_country, " (", min_year, " - ", max_year, ")"
+  )
+  
+  file_label <- paste0(
+    "mort_", this_country, "_(", min_year, "_", max_year, ")" 
+  )
+  
+  png(paste0("figures/hmd/mort/", file_label, ".png"),
+      res=300, width=40, height=20, units = "cm"
+  )
+  
+  
+  
+  p <- x %>% filter( age <= 90 & sex !="total") %>% 
+    mutate(mort_rate = death_count/ population_count) %>% 
+    contourplot(
+      mort_rate ~ year * age | sex, 
+      data=. , 
+      region=T, 
+      par.strip.text=list(cex=1.4, fontface="bold"),
+      ylab=list(label="Age in years", cex=1.4),
+      xlab=list(label="Year", cex=1.4),
+      cex=1.4,
+      cuts=20,
+      col.regions=colorRampPalette(brewer.pal(6, "Reds"))(200),
+      main=NULL,
+      labels=list(cex=1.2),
+      col="blue",
+      scales=list(
+        x=list(cex=1.4), 
+        y=list(cex=1.4),
+        alternating=3
+      )
+    )
+  
+  print(p)
+  dev.off()
+  
+  return(NULL)
+}
+
+
+d_ply(dta_hmd, .(country), spool_mort_figs)
+
+
+
+
+
+spool_lmort_figs <- function(x){
+  this_country <- x$country[1]
+  min_year <- min(x$year)
+  max_year <- max(x$year)
+  
+  title_label <- paste0(
+    this_country, " (", min_year, " - ", max_year, ")"
+  )
+  
+  file_label <- paste0(
+    "lmort_", this_country, "_(", min_year, "_", max_year, ")" 
+  )
+  
+  png(paste0("figures/hmd/lmort/", file_label, ".png"),
+      res=300, width=40, height=20, units = "cm"
+  )
+  
+  
+  
+  p <- x %>% filter( age <= 90 & sex !="total") %>% 
+    mutate(mort_rate = death_count/ population_count) %>% 
+    contourplot(
+      log(mort_rate) ~ year * age | sex, 
+      data=. , 
+      region=T, 
+      par.strip.text=list(cex=1.4, fontface="bold"),
+      ylab=list(label="Age in years", cex=1.4),
+      xlab=list(label="Year", cex=1.4),
+      cex=1.4,
+      cuts=20,
+      col.regions=colorRampPalette(brewer.pal(6, "Reds"))(200),
+      main=NULL,
+      labels=list(cex=1.2),
+      col="blue",
+      scales=list(
+        x=list(cex=1.4), 
+        y=list(cex=1.4),
+        alternating=3
+      )
+    )
+  
+  print(p)
+  dev.off()
+  
+  return(NULL)
+}
+
+
+d_ply(dta_hmd, .(country), spool_lmort_figs)
+
+
+
+
+dir.create("figures/hfd/asfr/", recursive = TRUE)
+dir.create("figures/hfd/cpfr/", recursive = TRUE)
+
+
+spool_asfr_figs <- function(x){
+  this_country <- x$code[1]
+  min_year <- min(x$year)
+  max_year <- max(x$year)
+  
+  title_label <- paste0(
+    this_country, " (", min_year, " - ", max_year, ")"
+  )
+  
+  file_label <- paste0(
+    "asfr_", this_country, "_(", min_year, "_", max_year, ")" 
+  )
+  
+  png(paste0("figures/hfd/asfr/", file_label, ".png"),
+      res=300, width=25, height=25, units = "cm"
+  )
+  
+  
+  
+  p <- x %>% filter( age <= 50 ) %>% 
+    contourplot(
+      asfr ~ year * age , 
+      data=. , 
+      region=T, 
+      par.strip.text=list(cex=1.4, fontface="bold"),
+      ylab=list(label="Age in years", cex=1.4),
+      xlab=list(label="Year", cex=1.4),
+      cex=1.4,
+      cuts=20,
+      col.regions=colorRampPalette(brewer.pal(6, "Purples"))(200),
+      main=NULL,
+      labels=list(cex=1.2),
+      col="black",
+      scales=list(
+        x=list(cex=1.4), 
+        y=list(cex=1.4),
+        alternating=3
+      )
+    )
+  
+  print(p)
+  dev.off()
+  
+  return(NULL)
+}
+
+
+d_ply(dta_hfd, .(code), spool_asfr_figs, .progress="text")
+
+
+spool_cpfr_figs <- function(x){
+  this_country <- x$code[1]
+  min_year <- min(x$year)
+  max_year <- max(x$year)
+  
+  title_label <- paste0(
+    this_country, " (", min_year, " - ", max_year, ")"
+  )
+  
+  file_label <- paste0(
+    "cpfr_", this_country, "_(", min_year, "_", max_year, ")" 
+  )
+  
+  png(paste0("figures/hfd/cpfr/", file_label, ".png"),
+      res=300, width=25, height=25, units = "cm"
+  )
+  
+  
+  
+  p <- x %>% filter( age <= 50 ) %>% 
+    contourplot(
+      cpfr ~ year * age , 
+      data=. , 
+      region=T, 
+      par.strip.text=list(cex=1.4, fontface="bold"),
+      ylab=list(label="Age in years", cex=1.4),
+      xlab=list(label="Year", cex=1.4),
+      cex=1.4,
+      cuts=20,
+      col.regions=colorRampPalette(brewer.pal(6, "Purples"))(200),
+      main=NULL,
+      labels=list(cex=1.2),
+      col="black",
+      scales=list(
+        x=list(cex=1.4), 
+        y=list(cex=1.4),
+        alternating=3
+      )
+    )
+  
+  print(p)
+  dev.off()
+  
+  return(NULL)
+}
+
+
+d_ply(dta_hfd, .(code), spool_cpfr_figs, .progress="text")
+
+
